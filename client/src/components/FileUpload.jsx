c28e612a860ec7f1889a221dae57d1d5aa87a00b
SURAJ <surajk38656@gmail.com>
2026-01-21 04:03:39 +0530

    FIXED issue for deployement ✅

--- client/src/components/FileUpload.jsx
+++ client/src/components/FileUpload.jsx
@@ -8,7 +8,7 @@ export default function Upload({ onResult }) {
         formData.append('file', file);
 
 
-        const res = await axios.post('http://localhost:5000/api/analyze', formData);
+        const res = await axios.post('https://first500days.onrender.com/api/analyze', formData);
         console.log(res.data, 'data')
         onResult(res.data);
     };
