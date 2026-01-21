9889b46424134b0fdaae852acae2a9e93227a501
SURAJ <surajk38656@gmail.com>
2026-01-21 23:25:59 +0530

    FIXED MONGODB ATLAS NETWORK ISSUE

--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import analyzeRoute from "./routes/analyze.route.js";
-import connectDB from "./lib/db.js";
-
+import connectDB from "./config/db.js";
 dotenv.config();
 
 const app = express();
+await connectDB()
 
 /* ✅ SAFE CORS */
 app.use(cors({ origin: "*" }));
@@ -17,15 +17,13 @@ app.use(express.urlencoded({ extended: true }));
 /* ✅ Health check */
 app.get("/", (req, res) => {
   res.send("SERVER IS RUNNING ✅");
+  // res.end();
 });
 
 /* ✅ Routes */
 app.use("/api/analyze", analyzeRoute);
 
 /* ✅ DB (NON-BLOCKING) */
-connectDB()
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("Mongo error:", err));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
